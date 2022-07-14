a90ee176638d70b97af56300b8911c2b0d288a6e
joallisson <joallissonnc@gmail.com>
2022-07-14 16:33:41 -0300

    feat: #05 - Configurando tema no MUI

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,7 +5,7 @@ export const AppRoutes = () => {
 
     return(
         <Routes>
-            <Route path="/pagina-inicial" element={<Button>Teste</Button>}/>
+            <Route path="/pagina-inicial" element={<Button color='primary' variant='contained'>Teste</Button>}/>
 
             <Route path='*' element={<Navigate to="/pagina-inicial"/>}/>
         </Routes>
